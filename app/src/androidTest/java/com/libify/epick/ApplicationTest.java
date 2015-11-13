6ba662e8f76aba04f7d7476e4da55334ba301346
Rom Shiri <roms12@gmail.com>
2015-11-13 13:15:41 +0200

    first commit

---
+++ app/src/androidTest/java/com/libify/epick/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.libify.epick;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/libify/epick/utils/IoC/ApiModule.java
@@ -0,0 +1,7 @@
+package com.libify.epick.utils.IoC;
+
+/**
+ * Created by romshiri on 13/11/15.
+ */
+public class ApiModule {
+}
