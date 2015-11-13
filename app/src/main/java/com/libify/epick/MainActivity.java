7d15ab7b0a5852608c523c84f32e63d4d85df595
Rom Shiri <roms12@gmail.com>
2015-11-13 18:46:18 +0200

    Add new list for main page

--- app/src/main/java/com/libify/epick/MainActivity.java
+++ app/src/main/java/com/libify/epick/MainActivity.java
@@ -4,10 +4,17 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.libify.epick.homePage.PickItemViewModel;
+import com.libify.epick.homePage.PicksAdapter;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -16,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.toolbar)
     Toolbar toolbar;
+
+    @Bind(R.id.picks_recycleView)
+    RecyclerView picksList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +44,37 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void initRecycleView(){
+        GridLayoutManager layoutManager = new GridLayoutManager(getApplicationContext(),2);
+        picksList.setLayoutManager(layoutManager);
+        picksList.setAdapter(new PicksAdapter(getDataFromTheServer()));
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    private ArrayList<PickItemViewModel> getDataFromTheServer(){
+        ArrayList<PickItemViewModel> results = new ArrayList<>();
+        ArrayList<String> images = new ArrayList<>();
+
+        images.add("https://lh4.googleusercontent.com/-wC54Rcr7Hq8/AAAAAAAAAAI/AAAAAAAAAAA/p8lQdwq1v6Y/s0-c-k-no-ns/photo.jpg");
+        images.add("https://lh4.googleusercontent.com/-wC54Rcr7Hq8/AAAAAAAAAAI/AAAAAAAAAAA/p8lQdwq1v6Y/s0-c-k-no-ns/photo.jpg");
+        images.add("https://lh4.googleusercontent.com/-wC54Rcr7Hq8/AAAAAAAAAAI/AAAAAAAAAAA/p8lQdwq1v6Y/s0-c-k-no-ns/photo.jpg");
+        images.add("https://lh4.googleusercontent.com/-wC54Rcr7Hq8/AAAAAAAAAAI/AAAAAAAAAAA/p8lQdwq1v6Y/s0-c-k-no-ns/photo.jpg");
+
+        results.add(new PickItemViewModel("What should I buy?", images));
+        results.add(new PickItemViewModel("What should I buy to my Dad?", images));
+        results.add(new PickItemViewModel("Which is better?", images));
+        results.add(new PickItemViewModel("What do you I should buy for my Parents?", images));
+        results.add(new PickItemViewModel("My girlfriend love gadgets, what should I buy?", images));
+        results.add(new PickItemViewModel("What should I buy?", images));
+
+        return results;
+
     }
 
     @Override
--- app/src/main/java/com/libify/epick/homePage/PickItemView.java
+++ app/src/main/java/com/libify/epick/homePage/PickItemView.java
@@ -2,11 +2,31 @@ package com.libify.epick.homePage;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.libify.epick.R;
+import com.squareup.picasso.Picasso;
+
+import butterknife.Bind;
 
 public class PickItemView extends RelativeLayout{
+    @Bind(R.id.imageLeft)
+    ImageView imageViewLeft;
+
+    @Bind(R.id.imageRight)
+    ImageView imageViewRight;
+
+    @Bind(R.id.imageBottomLeft)
+    ImageView imageViewBottomLeft;
+
+    @Bind(R.id.imageBottomRight)
+    ImageView imageViewBottomRight;
+
+    @Bind(R.id.pickTitle)
+    TextView title;
+
     public PickItemView(Context context) {
         super(context);
         initView();
@@ -23,7 +43,18 @@ public class PickItemView extends RelativeLayout{
     }
 
     private void initView(){
-        inflate(getContext(), R.layout.pick_item_layout,null);
+        inflate(getContext(), R.layout.pick_item_layout, null);
+    }
+
+    public void bindData(PickItemViewModel data){
+        Picasso.with(getContext()).load(data.getImageUrls().get(0)).into(imageViewLeft);
+        Picasso.with(getContext()).load(data.getImageUrls().get(1)).into(imageViewRight);
+        Picasso.with(getContext()).load(data.getImageUrls().get(2)).into(imageViewBottomLeft);
+        Picasso.with(getContext()).load(data.getImageUrls().get(3)).into(imageViewBottomRight);
+
+        title.setText(data.getTitle());
+
+
     }
 
 }
