8c118ac81df8e4a4d0471f78a874e138b57ec1e0
Rom Shiri <roms12@gmail.com>
2015-11-13 22:25:03 +0200

    sadasdsd

--- app/src/main/java/com/libify/epick/models/Pick.java
+++ app/src/main/java/com/libify/epick/models/Pick.java
@@ -1,7 +1,10 @@
 package com.libify.epick.models;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by ofeka_000 on 11/13/2015.
@@ -18,5 +21,8 @@ public class Pick {
         this.pickTitle = title;
         products = new ArrayList<Product>();
         isGenerated = false;
+        pickId = UUID.randomUUID().toString();
+
+        Log.d("ROM", "Created pick:" + pickId);
     }
 }
