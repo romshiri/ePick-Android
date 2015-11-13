a9ecaf3272fa98b4c70364f8048206f1756bf0b9
Rom Shiri <roms12@gmail.com>
2015-11-13 22:17:21 +0200

    alla kusamo eifo db

---
+++ app/src/main/java/com/libify/epick/CreateDilema.java
@@ -0,0 +1,46 @@
+package com.libify.epick;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.libify.epick.models.Pick;
+import com.libify.epick.storage.PicksStorage;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CreateDilema extends AppCompatActivity {
+
+    @Bind(R.id.create)
+    Button button;
+
+    @Bind(R.id.text_input)
+    EditText textEdit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_dilema);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ButterKnife.bind(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PicksStorage storage = PicksStorage.getInstance(getApplicationContext());
+                storage.addPick(new Pick(textEdit.getText().toString()));
+                finish();
+            }
+        });
+
+    }
+
+}
