d83e29c088cb7b4096c868eb88a5f39e3b2404c0
OfekAgmon <ofek.agmon@gmail.com>
2015-11-14 15:29:59 +0200

    fgfg

--- app/src/main/java/com/libify/epick/ShareScreen.java
+++ app/src/main/java/com/libify/epick/ShareScreen.java
@@ -4,6 +4,8 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -16,6 +18,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.libify.epick.R;
@@ -126,6 +129,7 @@ public class ShareScreen extends AppCompatActivity {
                         product.ebayId = id;
                         Picasso.with(ShareScreen.this)
                                 .load(product.imageUrl)
+                                .centerInside().resize(200, 200)
                                 .into(productImage);
                         productTitle.setText(product.productTitle);
                         productPrice.setText(product.productPrice + " $");
